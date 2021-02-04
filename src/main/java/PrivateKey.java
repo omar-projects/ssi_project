2c4bea27bdd409501b918aaadc3b22b15549949e
anas tagueniti <anas.tagueniti@etud.univ-angers.fr>
2021-02-04 15:17:53 +0100

    commentaire private key

--- src/main/java/PrivateKey.java
+++ src/main/java/PrivateKey.java
@@ -28,31 +28,29 @@ public class PrivateKey implements Serializable {
     	
 		BigInteger a = BigInteger.valueOf(publicKey.getE()); // a = e
 		BigInteger b = publicKey.getM(); // b = m			 
-
-		//BigInteger r; // r = a * u + b * v = pgcd(a, b) 
 	
 		//Initialisation
-		BigInteger r0 = a, r1 = b;
-		BigInteger u0 = BigInteger.ONE, u1 = BigInteger.ZERO;  
-		BigInteger v0 = BigInteger.ZERO, v1 = BigInteger.ONE; 
+		BigInteger r0 = a, r1 = b; 
+		BigInteger u0 = BigInteger.ONE, u1 = BigInteger.ZERO; // u 0 = 1 & u 1 = 0
+		BigInteger v0 = BigInteger.ZERO, v1 = BigInteger.ONE; // v 0 = 0 & v 1 = 1
 		
-		BigInteger q;									//quotient entier
-		BigInteger rs, us, vs;							//variables de stockages    	
+		BigInteger q; // quotient
+		BigInteger rs, us, vs; // rs = r i + 1 & us = u i + 1 & vs = v i + 1
 	
-		while(r1 != BigInteger.ZERO){
-			q = r0.divide(r1);
-			rs = r0; us = u0; vs = v0;
+		while(r1 != BigInteger.ZERO){ // Arrêt si r = 0
+			q = r0.divide(r1); // r i – 1 / r i
+			rs = r0; us = u0; vs = v0; 
 			r0 = r1; u0 = u1; v0 = v1;
-			r1 = rs.subtract(q.multiply(r1)); 
-			u1 = us.subtract(q.multiply(u1)); 
-			v1 = vs.subtract(q.multiply(v1));
+			r1 = rs.subtract(q.multiply(r1)); // r i – 1 - (r i – 1 / r i ) × r i 
+			u1 = us.subtract(q.multiply(u1)); // u i – 1 - (r i – 1 / r i ) × u i 
+			v1 = vs.subtract(q.multiply(v1)); // v i – 1 - (r i – 1 / r i ) × v i
 		}
 	
-		BigInteger k = BigInteger.valueOf(-1);
+		BigInteger k = BigInteger.valueOf(-1); // k = -1
 		
 		while(u0.compareTo(BigInteger.valueOf(2)) != 1 || u0.compareTo(b) != -1 ){
-			u0 = u0.subtract(k.multiply(b));
-			k = k.subtract(BigInteger.valueOf(-1));
+			u0 = u0.subtract(k.multiply(b)); // u - k × m avec b=m
+			k = k.subtract(BigInteger.valueOf(-1)); // k (-1, -2...)
 		}
 	
 		privateKey.setN(publicKey.getN());
